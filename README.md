726f8e527f64a281518947bb0e8d6e4ed61adec7
SampleUser0001 <ittimfn+github@gmail.com>
2021-07-20 10:05:04 +0900

    作成終了

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Binary_or_Text_Java
 バイナリファイルかテキストファイルかを判定する。
+
+## 実行
+
+``` sh
+mvn clean compile 
+mvn exec:java -Dexec.mainClass="sample.binarytotext.App" -Dexec.args="'${PWD}/src/main/java/sample/binaryortext/App.java'"
+mvn exec:java -Dexec.mainClass="sample.binarytotext.App" -Dexec.args="'${PWD}/target/classes/sample/binaryortext/App.class'"
+```
+
+## 参考
+
+- [\[Java\] テキストファイルかどうかを判別する:プロプログラマー](https://propg.ee-mall.info/%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0/java/java-%E3%83%86%E3%82%AD%E3%82%B9%E3%83%88%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%81%8B%E3%81%A9%E3%81%86%E3%81%8B%E3%82%92%E5%88%A4%E5%88%A5%E3%81%99%E3%82%8B/)
+
---
+++ src/main/java/sample/binaryortext/App.java
@@ -0,0 +1,35 @@
+package sample.binaryortext;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.IOException;
+
+/**
+ * 対象のファイルがバイナリかテキストかを調査する。
+ */
+public class App {
+  public static void main( String[] args ) {
+    System.out.println(String.format("%s is %s.", args[0], App.isTextFile(args[0]) ? "text" : "binary"));
+  }
+  /**
+	 * テキストファイルかどうかを判定する
+	 *
+	 * @param filePath テキストファイル
+	 * @return trueならテキストファイル falseならバイナリファイル
+	 */
+	public static boolean isTextFile(String filePath) {
+		try(InputStream in = Files.newInputStream(Paths.get(filePath))) {
+			byte[] b = new byte[1];
+			while (in.read(b, 0, 1) > 0) {
+				if (b[0] == 0) {
+					return false;
+				}
+			}
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
